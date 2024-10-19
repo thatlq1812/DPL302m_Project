c176dd46ff89abb370009ef1d2855218f8dcc158
Le Quang That <145110224+thatlq1812@users.noreply.github.com>
2024-10-19 12:24:45 +0700

    Refactor preprocessing code to resize images to 64x64 for improved efficiency and readability

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,22 +1,21 @@
 import os
-import cv2
 import time
 import yaml
+import multiprocessing
+import cv2
+import torch
 import pandas as pd
 import numpy as np
+import albumentations as A
+from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from tqdm import tqdm
-import albumentations as A
-import multiprocessing
-
-import torch
 from torchvision import transforms
 
 class DataProcessor:
     def __init__(self, config_path):
-        """Khởi tạo DataProcessor với đường dẫn config."""
+        """Create a new DataProcessor instance."""
         with open(config_path) as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -27,12 +26,12 @@ class DataProcessor:
         self.n_workers = multiprocessing.cpu_count()
         
     def load_raw_data(self):
-        """Đọc dữ liệu từ file CSV."""
+        """Read the raw data from the CSV file."""
         raw_csv_path = self.config['data']['raw_csv_path']
         self.data = pd.read_csv(raw_csv_path)
 
     def data_check(self):
-        """Kiểm tra dữ liệu để phát hiện các vấn đề."""
+        """Check the data for potential issues."""
         print('Data shape:', self.data.shape)
         print()
         # Show data types
@@ -69,7 +68,7 @@ class DataProcessor:
         
 
     def data_cleaning(self):
-        """Tiền xử lý dữ liệu bằng cách làm sạch và mã hóa."""
+        """Preprocess the data by cleaning, encoding labels, and adding image paths."""
         self.data.drop_duplicates(subset=['image_id'], inplace=True)
         self.data.dropna(subset=['age', 'sex', 'localization'], inplace=True)
 
@@ -77,13 +76,13 @@ class DataProcessor:
         self.data = self.data[self.data['sex'] != 'unknown']
         self.data = self.data[self.data['localization'] != 'unknown']
 
-        # Chuyển đổi 'age' sang int
+        # Convert age to integer
         self.data['age'] = self.data['age'].astype(int)
 
-        # Mã hóa nhãn
+        # Encode labels
         self._encode_labels()
         
-        # Thêm đường dẫn của ảnh
+        # Add image paths
         self.data['image_id'] = self.data['image_id'].apply(lambda x: os.path.join(self.images_path, x + '.jpg'))
         
         print('Data after cleaning:')
@@ -92,7 +91,7 @@ class DataProcessor:
         
 
     def _encode_labels(self):
-        """Mã hóa các nhãn cho các cột danh mục."""
+        """Encode the categorical labels using LabelEncoder."""
         label_encoder_dx = LabelEncoder()
         self.data['dx_code'] = label_encoder_dx.fit_transform(self.data['dx'])
         
@@ -105,7 +104,7 @@ class DataProcessor:
         label_encoder_localization = LabelEncoder()
         self.data['localization_code'] = label_encoder_localization.fit_transform(self.data['localization'])
 
-        """Lưu trữ các label encoder."""
+        """Save the label encoders to a file."""
         self.label_encoders = {
             'dx': label_encoder_dx,
             'dx_type': label_encoder_dx_type,
@@ -114,15 +113,19 @@ class DataProcessor:
         }
         encoder_file_name = self.config['encoder']['path'] + str(self.config['preprocessing']['resize'][0]) + 'x' + str(self.config['preprocessing']['resize'][1]) + '.pt'
         torch.save(self.label_encoders, encoder_file_name)
-
+    
+    def drop_unnecessary_columns(self):
+        """Drop unnecessary columns."""
+        self.data = self.data.drop(columns=['lesion_id', 'image_id', 'dx', 'dx_type', 'sex', 'localization']).astype(np.float32)
+        self.data.reset_index(drop=True, inplace=True)
 
     def split_data(self, test_size=0.2, random_state=42):
-        """Chia tách dữ liệu thành tập huấn luyện và tập kiểm tra."""
+        """Split the data into training and testing sets."""
         train_data, test_data = train_test_split(self.data, test_size=test_size, random_state=random_state)
         return train_data.reset_index(drop=True), test_data.reset_index(drop=True)
 
     def load_images_worker(self, batch):
-        """Worker để đọc ảnh trong một batch."""
+        """Worker function to load images from a batch."""
         images = []
         for index, row in batch.iterrows():
             image_path = row['image_id']
@@ -135,7 +138,7 @@ class DataProcessor:
         return images
 
     def load_images(self, data):
-        """Đọc tất cả ảnh từ dữ liệu."""
+        """Read all images from the dataset."""
         all_images = []
         batches = [data[i:i + self.batch_size] for i in range(0, len(data), self.batch_size)]
         with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
@@ -215,51 +218,100 @@ class DataProcessor:
 
         return augmented_data, augmented_images
     
+    def compute_img_mean_std_worker(self, batch_images):
+        img_w, img_h = self.resize_shape
+        batch_imgs = []
+
+        for image in batch_images:
+            img = cv2.resize(image, (img_w, img_h))
+            batch_imgs.append(img)
+
+        return np.array(batch_imgs)
+
+    def compute_img_mean_std(self, images):
+        means = np.zeros(3)
+        stdevs = np.zeros(3)
+        total_count = 0
+        # Split images into batches
+        batches = [images[i:i + self.batch_size] for i in range(0, len(images), self.batch_size)]
+
+        with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
+            futures = {executor.submit(self.compute_img_mean_std_worker, batch): batch for batch in batches}
+            for future in tqdm(as_completed(futures), total=len(futures), desc="Processing Batches"):
+                img_batch = future.result()
+                if img_batch is not None:
+                    img_batch = img_batch.astype(np.float32) / 255.0  # Normalize
+
+                    # Compute mean and std for each channel in the batch
+                    for i in range(3):  # RGB channels
+                        means[i] += img_batch[:, :, :, i].mean() * img_batch.shape[0]
+                        stdevs[i] += (img_batch[:, :, :, i] ** 2).mean() * img_batch.shape[0]
+
+                    total_count += img_batch.shape[0]  # Update total count
+
+        # Compute means and stds
+        means /= total_count
+        stdevs = np.sqrt(stdevs / total_count - means ** 2)
+        print('Means:', means)
+        print('Stdevs:', stdevs)
+
+        return means, stdevs
+    
     def convert_to_tensor(self, image):
-        """Chuyển đổi ảnh thành tensor."""
+        """Convert an image to a PyTorch tensor."""
         to_tensor = transforms.ToTensor()
         return to_tensor(image)
     
     def save_data(self, data, images, file_suffix):
-        """Lưu dữ liệu và ảnh vào file."""
+        """Save the data and images to a file."""
         # Convert images to tensors
         augmented_images = [self.convert_to_tensor(image) for image in images]
 
-        # Tạo đường dẫn tệp tin
+        # Compute mean and standard deviation
+        norm_mean, norm_std = self.compute_img_mean_std(images)
+        norm_mean = torch.tensor(norm_mean)
+        norm_std = torch.tensor(norm_std)
+
+        # Save the mean and standard deviation to a file
+        
+
+        # Create file paths
         suffix = str(self.resize_shape[0]) + 'x' + str(self.resize_shape[1]) + '_' + file_suffix
+        norm_file_path = os.path.join(self.config['data']['processed_path'], f"norm_mean_std{suffix}.pt")
         data_path = os.path.join(self.config['data']['processed_path'], f'HAM10000_{suffix}.pt')
         images_path = os.path.join(self.config['data']['processed_images_path'], f'images_{suffix}.pt')
 
-        # Tạo thư mục nếu nó không tồn tại
-        os.makedirs(os.path.dirname(data_path), exist_ok=True)  # Tạo thư mục cho dữ liệu
-        os.makedirs(os.path.dirname(images_path), exist_ok=True)  # Tạo thư mục cho hình ảnh
+        # If the directories don't exist, create them
+        os.makedirs(os.path.dirname(data_path), exist_ok=True)  # Create directory for data
+        os.makedirs(os.path.dirname(images_path), exist_ok=True)  # Create directory for images
 
-        # Lưu dữ liệu
+        # Save
         torch.save(data, data_path)
         torch.save(augmented_images, images_path)
+        torch.save({'mean': norm_mean, 'std': norm_std}, norm_file_path)
 
     def run(self):
-        """Chạy toàn bộ quy trình xử lý dữ liệu."""
+        """Main - Run the data processing pipeline."""
         start_time = time.time()
         self.load_raw_data()
         self.data_check()
         self.data_cleaning()
+        self.drop_unnecessary_columns()
         train_data, test_data = self.split_data()
         train_images = self.load_images(train_data)
         test_images = self.load_images(test_data)
 
         augmentation_counts = self.type_count(train_data, self.config['preprocessing']['augmentation']['ratio'])
         augmented_train_data, augmented_train_images = self.augment_images(train_data, train_images, augmentation_counts)
-        
-
         print('Augmented train data shape:', augmented_train_data.shape)
         print('Raw test data shape:', test_data.shape)
-
-        # Lưu dữ liệu đã tăng cường
+        
+        print("Saving data...")
         self.save_data(augmented_train_data, augmented_train_images, 'train')
         self.save_data(test_data, test_images, 'test')
-        print('Data processing completed in', round(time.time() - start_time, 2), 'seconds.')
         print('Data saved successfully.')
+        print('Data preprocessing completed in', round(time.time() - start_time, 2), 'seconds.')
+        
 
 if __name__ == "__main__":
     processor = DataProcessor(config_path='config.yaml')
@@ -282,6 +334,8 @@ type_count(): Tính số lần tăng cường cho từng loại bệnh.
 augment_image_worker(): Tăng cường ảnh cho một hàng.
 augment_images(): Tăng cường ảnh cho tất cả dữ liệu.
 convert_to_tensor(): Chuyển ảnh thành tensor.
+compute_img_mean_std_worker(): Tính giá trị trung bình và độ lệch chuẩn cho một batch ảnh.
+compute_img_mean_std(): Tính giá trị trung bình và độ lệch chuẩn cho tất cả ảnh.
 save_data(): Lưu dữ liệu và ảnh vào file.
 run(): Chạy toàn bộ quy trình xử lý dữ liệu.
 """
